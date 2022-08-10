f44aadf65f6656f5cba0e1bc73a76bfd09c1a414
leo <leonardo.belluomini.95@gmail.com>
2022-08-10 22:01:38 +0200

    update fro sync

---
+++ app/src/main/java/com/OTN/AreaElement.java
@@ -0,0 +1,60 @@
+package com.OTN;
+
+import java.io.*;
+
+public class AreaElement  {
+	private File osmfile;
+	private File polyFileBoundary;
+	private boolean complete = false;
+	private String areaName;
+
+	AreaElement (File osmfile ) {
+		if ( osmfile.exists() ){
+			this.osmfile = osmfile;
+		}	
+	}
+	AreaElement () {
+		File tempFile;
+		try {
+			tempFile = File.createTempFile("otnc", ".osm");
+		} catch ( IOException ex ) {
+			System.out.println(ex.toString());
+			return;
+		}
+		tempFile.deleteOnExit();
+		this.osmfile = tempFile;
+	}
+
+	public File getOsmFile(){
+		return this.osmfile;
+	}
+	public File getPolyFile(){
+		return this.polyFileBoundary;
+
+	}
+
+	public void setpolyBoundary( File polyFileBoundary ){
+		if ( polyFileBoundary.exists() && polyFileBoundary.getName().endsWith(".poly") ){
+			this.polyFileBoundary = polyFileBoundary;
+			this.complete = true;
+			// to do add real check ?
+		}
+	}
+	public void setName(String name){
+		this.areaName = name;
+	}
+	public String getName() {
+		return areaName;
+	}
+	public boolean isValid() {
+		if ( ! this.complete ){
+			return this.osmfile.exists();
+		} else {
+			return ( this.osmfile.exists() && this.polyFileBoundary.exists() );
+		}
+	}
+	public boolean isComplete() {
+		return complete;
+
+	}
+}
